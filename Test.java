5f9c0403266393de24ce6a61ea1d1ac23bdddba9
kapil Sharma <kapilsharma3812@gmail.com>
2023-01-19 13:13:42 +0530

    changes in main()

--- Test.java
+++ Test.java
@@ -1,5 +1,14 @@
 class Test{
-    public void m2(){
+	public static void main(String[] args) {
+		String s1="name";
+		System.out.println(s1.length());
+		
+		System.out.println(s1.toUpperCase());
+		System.out.println(s1.toLowerCase());
+		 
+	}
+	public void m2(){
         System.out.println("Hey inside m2()");
     }
+   
 }
